2d89c0c5e2824629c4d75d1dec66a4c8a9ed19e5
mattzteh <mattzteh@gmail.com>
2023-02-03 15:20:34 -0800

    create dynamic response to pathname for navbar

--- src/Components/NavBar/index.js
+++ src/Components/NavBar/index.js
@@ -1,9 +1,20 @@
 import './NavBar.css';
 
-import { NavLink } from 'react-router-dom';
+import { NavLink, useLocation } from 'react-router-dom';
 import { motion as m } from 'framer-motion';
 import { container, fadeUp } from '../../util/animations';
 const NavBar = () => {
+
+    const classNames = ['/', '/about', '/projects'];
+
+    const location = useLocation();
+    const navBarToggle = (className) => {
+        if (location.pathname === className) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 
     return (
         <>
@@ -11,15 +22,21 @@ const NavBar = () => {
             <m.div variants={container} initial="hidden" animate="show" className='links'>
                 
                 <m.div variants={fadeUp} className='link'>
-                    <NavLink exact to="/" style={{ textDecoration: 'none' }}><nav>Home</nav></NavLink>
+                    <NavLink exact to="/" style={{ textDecoration: 'none' }}>
+                        <nav className={navBarToggle(classNames[0]) ? 'onComponent' : '' }>Home</nav>
+                    </NavLink>
                 </m.div>
                 
                 <m.div variants={fadeUp} className='link'>
-                    <NavLink exact to="/about" style={{ textDecoration: 'none' }}><nav>About Me</nav></NavLink> 
+                    <NavLink exact to="/about" style={{ textDecoration: 'none' }}>
+                        <nav className={navBarToggle(classNames[1]) ? 'onComponent' : '' }>About Me</nav>
+                    </NavLink> 
                 </m.div>
 
                 <m.div variants={fadeUp} className='link'>
-                    <NavLink exact to="/projects" style={{ textDecoration: 'none' }}><nav>Projects</nav></NavLink>
+                    <NavLink exact to="/projects" style={{ textDecoration: 'none' }}>
+                        <nav className={navBarToggle(classNames[2]) ? 'onComponent' : '' }>Projects</nav>
+                    </NavLink>
                 </m.div>
             </m.div>
         </nav>
