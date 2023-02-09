b8316c7fee7801536c61b511d69d015188ee057a
mattzteh <mattzteh@gmail.com>
2023-02-09 13:38:25 -0800

    make about me component styling responsive

--- src/Components/About/index.js
+++ src/Components/About/index.js
@@ -1,5 +1,5 @@
 import './About.css';
-
+import aboutPic from '../../assets/aboutPic.jpg';
 import { motion as m } from 'framer-motion';
 
 const About = () => {
@@ -12,30 +12,34 @@ const About = () => {
     transition={{ duration: 0.75, ease: 'easeOut' }}
     exit={{ opacity: 0 }}
     className="comp-container">
-      <div className='about-content'>
+
+      <div className='about'>
         <h1>About Me</h1>
-        <p>
-          "Welcome to my website! My name is Matthew. I have a passion for Web Development and love 
-          creating and designing websites. I started my journey as a Software Engineer back in 2022, 
-          when I decided to make a career change from Business/Finance. From coding my first Hello 
-          World program, all the way to creating a website, I have truly rediscovered my love for 
-          learning and am hungry to always learn more and build my skillset. I am currently looking 
-          for opportunities to showcase my skills and be a part of a team that works on meaningful projects!"
-        </p>
-        <h2>My Skills:</h2>
-        <ul>
-          <li>JavaScript</li>
-          <li>Python</li>
-          <li>Ruby</li>
-          <li>Ruby on Rails</li>
-          <li>React.js</li>
-          <li>Node.js</li>
-          <li>Express.js</li>
-          <li>MongoDB</li>
-          <li>SQL</li>
-          <li>HTML/CSS</li>
-        </ul>
-      </div>  
+
+        <div className='about-content'>
+
+          <div className='about-text'>
+          <p>
+            Hi! My name is Matthew, and welcome to my website! I have a passion for Web Development and I love 
+            creating and designing websites.
+          </p>
+          <p>
+            My journey as a Software Engineer began back in 2022, 
+            when I decided I was not happy in the Business / Finance industry. From coding my first "Hello 
+            World" program to creating a fully fledged website, I have truly rediscovered my love for 
+            learning and am hungry to constantly learn more and build my skillset.
+          </p>
+          <p>
+            I am currently looking for opportunities to work as a Software Engineer and I am excited to be 
+            a part of a team that works on meaningful and impactful projects!
+          </p>
+          </div>
+
+          <img src={aboutPic} alt="matthew" />
+        </div>  
+      </div>
+
+
     </m.div>
     </>
     )
