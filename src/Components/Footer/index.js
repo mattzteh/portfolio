bcaa70573089fa3cffdbe46fe76bff928b3df370
mattzteh <mattzteh@gmail.com>
2023-02-10 14:23:09 -0800

    change margin and update footer styling

--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -7,34 +7,34 @@ const Footer = () => {
     return (
         <>
         <div className='footer'>
-            <m.div variants={container} initial="hidden" animate="show" className='footer-links'>
+            <m.ul variants={container} initial="hidden" animate="show" className='footer-links'>
 
-                <m.div variants={fadeIn} className="socials">
+                <m.li variants={fadeIn} className="socials">
                     <a href='https://github.com/mattzteh' target="_blank" rel="noreferrer">
                         <i class="fa-brands fa-github"></i>
                     </a>
-                </m.div>
+                </m.li>
 
-                <m.div variants={fadeIn} className="socials">
+                <m.li variants={fadeIn} className="socials">
                     <a href='https://www.linkedin.com/in/mattzteh/' target="_blank" rel="noreferrer">
                         <i class="fa-brands fa-linkedin-in"></i>
                     </a>
-                </m.div>
+                </m.li>
 
-                <m.div variants={fadeIn} className="socials">
+                <m.li variants={fadeIn} className="socials">
                     <a href='https://angel.co/u/matthew-teh-1' target="_blank" rel="noreferrer">
                     <i class="fa-brands fa-angellist"></i>
                     </a>
-                </m.div>
+                </m.li>
 
-                <m.div variants={fadeIn} className="socials">
+                <m.li variants={fadeIn} className="socials">
                     <a href='mailto: mattzteh@gmail.com'>
                         <i class="fa-solid fa-envelope"></i>
                     </a>
-                </m.div>
+                </m.li>
 
-                <m.div variants={fadeUp} className='vl'></m.div>
-            </m.div>
+                <m.li variants={fadeUp} className='vl'></m.li>
+            </m.ul>
         </div>
         </>
     )
