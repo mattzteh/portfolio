5313521d9e45b538f525f9a155f6ccccfd403c50
mattzteh <mattzteh@gmail.com>
2023-02-10 18:48:24 -0800

    start change about content

--- src/Components/About/index.js
+++ src/Components/About/index.js
@@ -1,5 +1,4 @@
 import './About.css';
-import aboutPic from '../../assets/aboutPic.jpg';
 import { aboutText } from '../../assets/text';
 
 import { useEffect } from 'react';
@@ -9,13 +8,13 @@ import { useInView } from 'react-intersection-observer';
 const About = () => {
 
 	const animation = useAnimation();
-	const {ref, inView} = useInView({threshold: 0.2});
+	const {ref, inView} = useInView({ threshold: 0.2 });
 
 	useEffect(() => {
 		if (inView) {
-			animation.start({opacity: 1, transition: {delay: 0.2}})
+			animation.start({ opacity: 1, y: '0%', transition: { delay: 0.2, bounce: 0 }})
 		} else {
-			animation.start({opacity: 0, transition: {delay: 0.2}})
+			animation.start({ opacity: 0, y: '10%', transition: { delay: 0.2, bounce: 0 }})
 		}
 	}, [inView, animation]);
 
@@ -23,15 +22,15 @@ const About = () => {
     <>
     <div className="comp-container">
 		<m.div ref={ref} animate={animation} className='about'>
-    		<h1>About Me </h1>
+    		<h1>Welcome to my Website!</h1>
         	<div className='about-content'>
           		<div className='about-text'>
 					<p>{aboutText[0]}</p>
 					<p>{aboutText[1]}</p>
 					<p>{aboutText[2]}</p>
+					<p><a href='mailto: mattzteh@gmail.com'>Hire me!</a></p>
           		</div>
-          		<img src={aboutPic} alt="matthew" />
-        	</div>  
+        	</div>
       	</m.div>
     </div>
     </>
