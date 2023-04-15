ba7fdf6f8792f8e356e2bc73886dd7eaf4167bc9
mattzteh <mattzteh@gmail.com>
2023-04-15 15:14:42 -0700

    fix aws link for resume

--- src/assets/text.js
+++ src/assets/text.js
@@ -19,7 +19,7 @@ that works on meaningful and impactful projects!</m.p>
 
 export const aboutText = [p1, p2, p3];
 
-export const resumeLink = 'https://s3.amazonaws.com/attachments.angel.co/8236919-50f48a9ac432ecf40c9f0dd475a67517.pdf?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAJS6W3HGZGRJIRBTA%2F20230329%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20230329T003925Z&X-Amz-Expires=3600&X-Amz-SignedHeaders=host&X-Amz-Signature=9afb9cf75d379f5c4e7788804283d8e2fa762d483c38283c1a31391e64c4bdae';
+export const resumeLink = 'https://zzyworks-dev.s3.us-west-1.amazonaws.com/Matthew+Teh+Resume.docx.pdf';
 
 export const contactP1 = <m.p variants={fadeIn}>I am currently open to any <span>full-time</span>, <span>part-time</span>, 
 or <span>freelance</span> roles. If you would like to discuss opportunities, or just want to chat, feel free to send me an 
